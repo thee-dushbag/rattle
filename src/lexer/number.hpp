cc7f097356ac772dda4d217d0bf1bde0f34ebd08
Simon Nganga <theedushbag@gmail.com>
2024-07-01 00:11:57 +0300

    Cleaned up the number lexer draft
    The lexer is officially complete, maybe a little duck tape to hope and
    piece everything together under one class: Lexer. Otherwise, we have a
    fully functional lexer with some good error reporting and detection.
    Next phase, parser.

--- src/lexer/number.hpp
+++ src/lexer/number.hpp
@@ -1,128 +1,163 @@
 #pragma once
 
 #include "state.hpp"
+#include <string>
+#include <functional>
 #include "tokens.hpp"
 #include "utility.hpp"
 
 namespace rat::lexer {
-  // facade of how numbers are lexed
   namespace number_lexer {
-    namespace consume {
-      inline auto _if(State& S, bool (* const predicate)(char)) {
-        std::string::iterator start = S.current;
-        while ( not S.empty() and predicate(S.peek()) ) S.advance();
-        return S.current - start;
-      }
-
-      inline auto b2(State& S) {
-        return _if(S, utils::isbinary);
-      }
-      inline auto b8(State& S) {
-        return _if(S, utils::isoctal);
-      }
-      inline auto b10(State& S) {
-        return _if(S, utils::isdecimal);
-      }
-      inline auto b16(State& S) {
-        return _if(S, utils::ishexadecimal);
-      }
+    inline auto consume_if(State& S, const std::function<bool(char)> predicate) {
+      std::string::iterator start = S.current;
+      while ( not S.empty() and predicate(S.peek()) ) S.advance();
+      return S.current != start;
     }
 
-    namespace ns {
-      std::pair<Token::Kind, std::size_t>
-        number_separator(State& S, bool(* const checker)(char), Token::Kind kind, const char* msg) {
-        std::size_t total{ 0 };
-        for ( ; not S.empty();) {
-          total += consume::_if(S, checker);
-          if ( S.safe(2) and S.peek() == '_' ) {
-            if ( checker(S.peek(1)) ) S.erase();
-            else if ( S.peek(1) == '_' ) kind = (S.advance(), S.report_error(msg,
-              "Make sure all characters in the literals belong to the base of the literal "
-              "and there is atmost one separator between each."
-            ), Token::Kind::Error);
-            else break;
-          } else break;
+    inline bool separator(State& S,
+      std::function<bool(char)> predicate,
+      std::function<void(State&)> onerror) {
+      auto consumer = [&] (State& S) -> bool {
+        return consume_if(S, predicate);
+      };
+      auto start = S.current;
+      for ( ; not S.empty();) {
+        if ( not consumer(S) ) { onerror(S); break; }
+        if ( S.safe(2) and
+          S.peek() == '_' and
+          predicate(S.peek(1)) )
+          S.erase();
+      }
+      return start != S.current;
+    }
+
+    inline Token::Kind check_literals(State& S,
+      Token::Kind kind,
+      std::function<bool(char)> const predicate,
+      std::string_view const invalid_literal_msg,
+      std::string_view const fix = "") {
+      return separator(S, predicate, [&] (State& S) {
+        auto start_loc = S.current_location();
+        if ( consume_if(S, utils::isidchar) ) {
+          kind = Token::Kind::Error;
+          S.report_error_at(start_loc,
+            S.current_location(),
+            invalid_literal_msg, fix);
         }
-        return { kind, total };
-      }
-
-      auto b2(State& S) {
-        return number_separator(
-          S, utils::isbinary,
-          Token::Kind::Binary,
-          "Invalid binary literal."
-        );
-      }
-      auto b8(State& S) {
-        return number_separator(
-          S, utils::isoctal,
-          Token::Kind::Octal,
-          "Invalid octal literal."
-        );
-      }
-      auto b10(State& S) {
-        return number_separator(
-          S, utils::isdecimal,
-          Token::Kind::Decimal,
-          "Invalid decimal literal."
-        );
-      }
-      auto b16(State& S) {
-        return number_separator(
-          S, utils::ishexadecimal,
-          Token::Kind::Hexadecimal,
-          "Invalid hexadecimal literal."
-        );
-      }
+      }) ? kind : Token::Kind::Eot;
     }
+
+    Token::Kind base16(State& S) {
+      return check_literals(S,
+        Token::Kind::Hexadecimal,
+        utils::ishexadecimal,
+        "Invalid base 16 character literal",
+        "did you accidentally join an identifier to a number literal "
+        "or use invalid characters in a base 16 number literal"
+      );
+    }
+    Token::Kind base10(State& S) {
+      return check_literals(S,
+        Token::Kind::Decimal,
+        utils::isdecimal,
+        "Invalid base 10 character literal",
+        "did you accidentally join an identifier to a number literal "
+        "or use invalid characters in a base 10 number literal"
+      );
+    }
+    Token::Kind base8(State& S) {
+      return check_literals(S,
+        Token::Kind::Octal,
+        utils::isoctal,
+        "Invalid base 8 character literal",
+        "did you accidentally join an identifier to a number literal "
+        "or use invalid characters in a base 8 number literal"
+      );
+    }
+    Token::Kind base2(State& S) {
+      return check_literals(S,
+        Token::Kind::Binary,
+        utils::isbinary,
+        "Invalid base 2 character literal",
+        "did you accidentally join an identifier to a number literal "
+        "or use invalid characters in a base 2 number literal"
+      );
+    }
+
 
     Token::Kind consume_int_float(State& S) {
+      auto kind = base10(S);
+      if ( kind != Token::Kind::Decimal ) return kind;
+      if ( not S.empty() and S.match('.') ) {
+        kind = Token::Kind::Floating;
+
+      }
+      return kind;
+    }
+
+
+    Token::Kind base10f(State& S) {
+      auto consume = [] (State& S) {
+        return separator(S, utils::isdecimal, [] (State&) { });
+      };
+
       auto kind = Token::Kind::Decimal;
-      ns::b10(S);
+      consume(S);
 
       if ( not S.empty() and S.match('.') )
-        if ( not ns::b10(S).second ) kind = (
+        if ( not consume(S) ) kind = (
+          base10(S),
           S.report_error(
             "Expected at least one digit after decimal point.",
             "Put at least one digit after the decimal point."
-          ), Token::Kind::Error
-          ); else kind = Token::Kind::Floating;
+          ), Token::Kind::Error);
+        else kind = Token::Kind::Floating;
 
       if ( not S.empty() and (S.match('e') or S.match('E')) ) {
         kind = (
           not S.empty() and (S.match('+') or S.match('-')),
           Token::Kind::Floating
           );
-        if ( not ns::b10(S).second ) kind = (
+        if ( not consume(S) ) kind = (
+          base10(S),
           S.report_error(
             "Missing exponent value.",
             "Put at least one digit in the exponent section."
           ), Token::Kind::Error
           );
       }
-      return kind;
+      return base10(S) == Token::Kind::Error ?
+        Token::Kind::Error : kind;
     }
 
     Token::Kind consume_number(State& S) {
-      if ( S.match('0') and not S.empty() )
-        switch ( S.peek() ) {
-        case 'x':
-          return (S.advance(), ns::b16(S).second) ? Token::Kind::Hexadecimal : (
-            S.report_error("At least one hexadecimal value is expected after '0x'"),
-            Token::Kind::Error
-            );
-        case 'o':
-          return (S.advance(), ns::b8(S)).second ? Token::Kind::Octal : (
-            S.report_error("At least one octal value is expected after '0o'"),
-            Token::Kind::Error
-            );
+      auto start_loc = S.current_location();
+      const char* msg = nullptr;
+      if ( S.safe(2) and S.peek() == '0' ) {
+        Token::Kind kind = Token::Kind::Semicolon;
+        switch ( S.peek(1) ) {
         case 'b':
-          return (S.advance(), ns::b2(S)).second ? Token::Kind::Binary : (
-            S.report_error("At least one binary value is expected after '0b'"),
-            Token::Kind::Error
-            );
+          kind = (S.advance(), S.advance(), base2(S));
+          msg = "At least one binary literal is expected after 0b";
+          break;
+        case 'o':
+          kind = (S.advance(), S.advance(), base8(S));
+          msg = "At least one octal literal is expected after 0o";
+          break;
+        case 'x':
+          kind = (S.advance(), S.advance(), base16(S));
+          msg = "At least one hexadecimal literal is expected after 0x";
+          break;
         }
-      return consume_int_float(S);
+        switch ( kind ) {
+        case Token::Kind::Eot:
+          return (S.report_error(msg), Token::Kind::Error);
+        case Token::Kind::Semicolon:
+          break;
+        default: return kind;
+        }
+      }
+      return base10f(S);
     }
   }
 
